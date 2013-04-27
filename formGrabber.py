876fc7cb323ab953670806379d61418ec14e779d
eddiem3 <eeem33@case.edu>
2013-04-27 15:01:15 -0400

    Added basis for the form grabber

--- formGrabber.py
+++ formGrabber.py
@@ -1,3 +1,13 @@
 import cgi, cgitb
 
+#Instance of field storage object
 form = cgi.FieldStorage()
+
+
+#Get data from forms
+firstName = form.getvalue('first')
+lastName = form.getvalue('last')
+address = form.getvalue('e-mail')
+phone = form.getvalue('phone')
+summary = form.getvalue('summary')
+attachments = form.getvalue('attachments')

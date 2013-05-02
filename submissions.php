00bfd4b9302c020fbb72cd02ca16e91f99424f8a
eddiem3 <eeem33@case.edu>
2013-05-02 14:29:27 -0400

    Wrote a function to email the submission

--- submissions.php
+++ submissions.php
@@ -1,4 +1,13 @@
+
 <?php
+
+//initalizations
+require("lib/phpMailer/class.phpmailer.php");
+
+define('TO', 'eddiemasseyiii@gmail.com');
+
+set_time_limit(0);
+
 if($_POST['formSubmit'] == "Submit")
 {			
 			//Validate the form
@@ -44,19 +53,56 @@ if($_POST['formSubmit'] == "Submit")
 			//Parse data
 			$firstName = $_POST['firstname'];
 			$lastName = $_POST['lastname'];
+			$fullName = $firstName . $lastName;
 			$email = $_POST['e-mail'];
-			$descr = $_POST['description'];
+			$request = $_POST['description'];
+			$subject = "New GRID Submssion";
+			sendEmail(TO, $email, $fullName, $subject, $request);
 
-			echo "Firstname is $firstName";
-			echo "Lastname is $lastName";
-			echo "Email is $email";
-			echo "Description is $descr";
-
-
-
-
-			
 }
+
+/*
+*@desc Sends the form data
+*@param form submitter email
+*@parma form submitter name
+*@param subject
+*@param message body
+*/ 
+
+function sendEmail($to, $from, $fromName, $subject, $body)
+{
+	/* Get user name 
+	and password */
+
+	$lines = file("login"); //read the file called login into an array	
+	$userName = $lines[0];
+	$password = $lines[1];
+
+	 global $error;
+	 $mail = new PHPMailer();  // create a new object
+	 $mail->IsSMTP(); // enable SMTP
+	 $mail->SMTPDebug = 0;  // debugging: 1 = errors and messages, 2 = messages only
+	 $mail->SMTPAuth = true;  // authentication enabled
+	 $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for GMail
+	 $mail->Host = 'smtp.gmail.com';
+	 $mail->Port = 465; 
+	 $mail->Username = $userName;  
+	 $mail->Password = $password;           
+	 $mail->SetFrom($from, $fromName);
+	 $mail->Subject = $subject;
+	 $mail->Body = $body;
+	 $mail->AddAddress($to);
+	 if(!$mail->Send()) {
+	 		    $error = 'Mail error: '.$mail->ErrorInfo; 
+			    	   return false;
+				   } else {
+				     $error = 'Message sent!';
+				     	    return true;
+					    }
+}
+
+	
+
 
 
 
